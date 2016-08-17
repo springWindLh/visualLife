178b9dd1a58ad60ebda08f8116e2797321dad688
luohuan <297750341@qq.com>
2016-08-17 18:09:55 +0800

    富文本编辑器引入

--- src/main/java/lh/world/controller/ArticleController.java
+++ src/main/java/lh/world/controller/ArticleController.java
@@ -1,10 +1,13 @@
 package lh.world.controller;
 
 import lh.world.controller.support.BaseController;
+import lh.world.form.ArticleForm;
 import lh.world.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by lh on 2016/8/11.
@@ -15,4 +18,9 @@ public class ArticleController extends BaseController {
     @Autowired
     ArticleService articleService;
 
+    @RequestMapping(value = "/add",method = RequestMethod.GET)
+    public String add(Model model) {
+        model.addAttribute("form", new ArticleForm());
+        return "/article/form";
+    }
 }
